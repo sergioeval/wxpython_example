1c8f82c0faf3a7d3eda1ddc9011a17a62ff7fbe3
SERGIO ESTRADA VALENZUELA <sergioev@mx1.ibm.com>
2020-01-16 16:16:15 -0600

    updates

---
+++ constants.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 *-*
+APP_TITLE = "Creador de Cartas Comerciales"
+CONFIRM_TITLE = "Archivo guardado con éxito!"
+CONFIRMAR = "El archivo se ha guardado exitosamente en cartas/"
+MENU_GUARDAR = "&Guardar"
+STATUS_GUARDAR = "Guarda el documento como nueva carta"
+MENU_ABOUT = "&Acerca de"
+STATUS_ABOUT = "Información sobre el programa"
+MENU_SALIR = "&Salir"
+STATUS_SALIR = "Finaliza el programa"
+MENU_TITLE = "&Documento"
+ABOUT_TITLE = "Acerca de"
+ABOUT_CONTENT = """Generador de cartas comerciales simples con pre-formato,
+basado en wxPython.
+
+Creado en el capítulo 8 de la Guía Python de Maestros del Web.
+http://www.maestrosdelweb.com/editorial/guia-python/
+
+Diciembre de 2011"""
+
+TEMPLATE = """
+    EMPRESA
+    DOMICILIO
+
+                                         LUGAR, FECHA
+
+    Lic. NOMBRE APELLIDO
+    S.       /        D.
+
+
+        Tengo el agrado de dirigirme a usted, a fin de hacerle llegar una
+    nueva oferta promocional para el servicio de SERVICIO.-
+
+        En el folleto adjunto, podrá encontrar más detalles de la misma.-
+
+        Por favor, ante cualquier inquietud, siéntase libre de contactarme
+    telefónicamente al 4444-4444 int. 4444 o por e-mail a
+    user@mail.com.-
+
+        A la espera de que esta oferta sea de su agrado, le hago llegar un
+    cálido saludo.-
+
+
+                                            Juan Pérez
+                                            www.juanperez.com
+"""
